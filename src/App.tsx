c6ae4d7a9129a8454bd1db19fc8991259efa0ce4
Gary Varner <garysarahvarner@gmail.com>
2024-05-07 22:21:52 -0400

    starting a simple golf site

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Button } from "@/components/ui/button";
+import { EventList } from "@/components/event-list";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <EventList />
+    </>
+  );
+}
+
+export default App;
